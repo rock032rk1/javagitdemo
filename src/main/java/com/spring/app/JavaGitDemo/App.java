e44ab1f3fcff988512f6b5b7b8181bfd32419701
Rakesh <rakeshmaraskolhe08@gmail.com>
2020-03-01 15:36:43 +0530

    Student Addedd

--- src/main/java/com/spring/app/JavaGitDemo/App.java
+++ src/main/java/com/spring/app/JavaGitDemo/App.java
@@ -3,9 +3,11 @@ package com.spring.app.JavaGitDemo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class App 
 {
+	public static Student st=new Student();
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
@@ -19,5 +21,15 @@ public class App
         }
         
         System.out.println("Java Git demo Child branch of Master branch");
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Name :");
+        st.setName(sc.nextLine());
+        System.out.println("Enter the Mobile No");
+        st.setMobile(sc.nextLine());
+        
+        System.out.println("Student Name :"+st.getName());
+        System.out.println("Student Mobile No :"+st.getMobile());
+        
     }
 }
---
+++ src/main/java/com/spring/app/JavaGitDemo/Student.java
@@ -0,0 +1,35 @@
+package com.spring.app.JavaGitDemo;
+
+public class Student {
+
+	private String name;
+	private String mobile;
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, String mobile) {
+		super();
+		this.name = name;
+		this.mobile = mobile;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	
+}
