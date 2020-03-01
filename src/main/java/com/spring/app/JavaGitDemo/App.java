e84b7d4c88dbff5b9f7a642b1e001063278f255c
Rakesh <rakeshmaraskolhe08@gmail.com>
2020-03-01 13:21:27 +0530

    Second commit

--- src/main/java/com/spring/app/JavaGitDemo/App.java
+++ src/main/java/com/spring/app/JavaGitDemo/App.java
@@ -1,11 +1,21 @@
 package com.spring.app.JavaGitDemo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         
+        List<String> slist=new ArrayList<String>();
+        slist.add("Rakesh");
+        slist.add("Satish");
         
+        for(String s:slist) {
+        	System.out.println(s);
+        }
     }
 }
